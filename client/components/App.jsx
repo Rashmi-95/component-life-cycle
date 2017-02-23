6021043d7d1beb962f9623462e4191a7528390a3
rashmi-95 <rashmi05rash@gmail.com>
2017-02-23 13:22:43 +0530

    example

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,61 @@
 import React from 'react';
-import video from '../Video.jsx';
-import videoList from '../VideoList.jsx';
+import ReactDOM from 'react-dom'
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+let App = React.createClass({
+  getDefaultProps: function () {
+    console.log('getDefaultProps : default props empty')
+    return {}
+  },
+  getInitialState: function () {
+    console.log('getInitialState : default state count 0')
+    return {
+      count: 0
+    }
+  },
+  increase: function () {
+    this.setState({
+      count: this.state.count + 1
+    })
+  },
+  componentWillMount: function () {
+    console.log('componentWillMount: component before rendering frist time')
+  },
+  componentDidMount: function () {
+    console.log('ComponentDidMount: component after rendering first time')
+  },
+  componentWillReceiveProp: function () {
+    console.log('ComponentWillReceiveProp: component will prop cahnges')
+  },
+  shouldComponentUpdate: function (newProp, newState) {
+    console.log('shouldComponentUpdate: will return bool that handles rendering. Should update ?')
 
-  render() {
+    if (newState.count < 2) {
+      console.log('shouldComponentUpdate: should')
+      return true;
+    } else {
+      ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+      console.log('shouldComponentUpdate: should not')
+      return false;
+    }
+  },
+  componentWillUpdate: function () {
+    console.log('componentWillUpdate: component before rendering')
+  },
+  componentDidUpdate: function () {
+    console.log('componentDidUpdate: component updated')
+  },
+  componentWillUnmount: function () {
+    console.log('componentWillUnmount: component is to be removed')
+  },
+  render: function () {
+    console.log('render')
     return (
-      <div>
-        <h1>Book Library</h1>
+      <div id="counter">
+        <p> {this.state.count}</p>
+        <button onClick={this.increase}> + </button>
       </div>
     )
   }
-}
+})
+
+module.exports = App
---
+++ client/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Counter extends React.Component {
+ constructor(props){
+   super(props);
+ }
+ render() {
+   return (
+     <div style={{ textAlign: 'center' }}>
+       counter
+     </div>
+   );
+ }
+}
